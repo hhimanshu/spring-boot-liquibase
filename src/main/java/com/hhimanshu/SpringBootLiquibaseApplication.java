0b421d0f1f39a16bcac9447db826c62064dc9dae
hhimanshu <harit.himanshu@yahoo.in>
2017-05-17 15:43:31 +1200

    initial commit

---
+++ src/main/java/com/hhimanshu/SpringBootLiquibaseApplication.java
@@ -0,0 +1,12 @@
+package com.hhimanshu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLiquibaseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hhimanshu/SpringBootLiquibaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hhimanshu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootLiquibaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
