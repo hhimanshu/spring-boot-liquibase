fe3bff3013926cab2b73d47d3638d3018a7907da
Faceless <46992738+Snowmoan@users.noreply.github.com>
2019-06-11 21:02:27 +0200

    YL - Changed id-Generation strategy to IDENTITY and added autoIncrement=true to id-column, for Book and Author

--- src/main/java/com/hhimanshu/persistence/entities/Book.java
+++ src/main/java/com/hhimanshu/persistence/entities/Book.java
@@ -14,7 +14,7 @@ import javax.persistence.Table;
 public class Book {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
   @Column(nullable = false, unique = true)
